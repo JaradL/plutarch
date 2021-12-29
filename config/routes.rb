79cff5b555405f2864437ad7d5d685b63e310f0d
Jarad Little <jaradl@hotmail.co.uk>
2021-12-29 20:27:56 +1000

    PLUT-21: Hide/show content to signed in users (#8)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  root 'pages#home'
+  unauthenticated do
+    root 'pages#home', as: nil
+  end
+  
+  authenticated do
+    root 'dashboard#index'
+  end
 
   get 'features', to: 'pages#features'
 
@@ -8,4 +14,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
 
   devise_for :user, :path => '', :path_names => { :sign_in => "signin", :sign_out => "signout", :sign_up => "signup" }, controllers: { registrations: "registrations" }
+
+  resource :dashboard, :controller => 'dashboard'
+  get 'dashboard/index'
 end
